7afba05aa31f891e3bdd8eb87a1f4bd0389cc349
Michael Hutchings <mhutchings0416@gmail.com>
2024-02-26 09:11:12 -0700

    Add project files.

---
+++ Mission8_Sec2_Group1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission8_Sec2_Group1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
