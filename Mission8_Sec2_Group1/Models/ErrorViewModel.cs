c21f7686f1f9c0d9b663f5b301681f21e0ab32ea
dsrasband@gmail.com <dsrasband@gmail.com>
2024-02-26 21:41:36 -0700

    Claiming my branch

--- Mission8_Sec2_Group1/Models/ErrorViewModel.cs
+++ Mission8_Sec2_Group1/Models/ErrorViewModel.cs
@@ -2,8 +2,12 @@ namespace Mission8_Sec2_Group1.Models
 {
     public class ErrorViewModel
     {
+        
+        // This is Davis' section
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public string Name { get; set; }
     }
 }
