3b7b0d1b14682f202f91e6e31d5dfaa15ea86782
Harrison Murphy Stone <harryway@byu.edu>
2024-02-27 09:26:27 -0700

    viewbag

--- Mission8_Sec2_Group1/Controllers/HomeController.cs
+++ Mission8_Sec2_Group1/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace Mission8_Sec2_Group1.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Quadrants()
+        {
+            ViewBag
+        }
     }
 }
