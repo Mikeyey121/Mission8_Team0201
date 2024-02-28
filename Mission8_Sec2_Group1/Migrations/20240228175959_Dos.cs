5fcc525aa475e2a67360d301d69406604aced227
Michael Hutchings <mhutchings0416@gmail.com>
2024-02-28 11:08:07 -0700

    commit

---
+++ Mission8_Sec2_Group1/Migrations/20240228175959_Dos.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission8_Sec2_Group1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Dos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TaskName",
+                table: "Tasks",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryName",
+                table: "Categories",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TaskName",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryName",
+                table: "Categories",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
