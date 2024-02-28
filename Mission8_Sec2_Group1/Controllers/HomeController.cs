3ac3ca70cc412a45944349190cb1fada98090eff
Harrison Murphy Stone <harryway@byu.edu>
2024-02-28 09:13:26 -0700

    commit

--- Mission8_Sec2_Group1/Controllers/HomeController.cs
+++ Mission8_Sec2_Group1/Controllers/HomeController.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using TaskModel.Models;
 using System.Diagnostics;
+using SQLitePCL;
+using Microsoft.EntityFrameworkCore;
+using HabitContext.Models;
 
 namespace Mission8_Sec2_Group1.Controllers
 {
     public class HomeController : Controller
     {
+        private HabitsContext _context;
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -23,9 +28,49 @@ namespace Mission8_Sec2_Group1.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult AddTask()
+        {
+            ViewBag.Tasks = _context.Tasks
+                .OrderBy(x => x.TaskId).ToList();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(AddTask task)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.AddTask.Add(task);
+                _context.SaveChanges();
+            }
+            else
+            {
+                ViewBag.Categories = _context.Tasks
+                    .OrderBy(x => x.TaskId).ToList();
+
+                return View(task);
+            }
+
+            return RedirectToAction("Quadrants");
+        }
+
+        [HttpGet]
+        public IActionResult Quadrants() 
+        {
+            return View();
+        }
+
+        [HttpPost]
         public IActionResult Quadrants()
         {
-            ViewBag
+            var tasks = _context.Tasks
+                .Include(x => x.Category)
+                .OrderBy(x => x.TaskId).ToList();
+
+
+            return View(tasks);
         }
     }
 }
